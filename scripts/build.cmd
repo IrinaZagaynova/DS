09c15dbe83e1710df7dc417fe328f28b1dada38f
IrinaZagaynova <ayrin.zagaynova@gmail.com>
2021-04-12 13:07:27 +0300

    Разместила команды сборки в start.cmd #4

--- scripts/build.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-SET VALUATOR_PATH=%~dp0..\Valuator\
-SET RANK_CALCULATOR_PATH=%~dp0..\RankCalculator\
-SET EVENTS_LOGGER_PATH=%~dp0..\EventsLogger\
-
-start /d%VALUATOR_PATH% dotnet build
-start /d%RANK_CALCULATOR_PATH% dotnet build
-start /d%EVENTS_LOGGER_PATH% dotnet build
