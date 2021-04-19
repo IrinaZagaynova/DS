83440b47aa846f17d523dc87a883e58f1dfa293e
IrinaZagaynova <55958101+IrinaZagaynova@users.noreply.github.com>
2021-04-19 19:40:32 +0300

    Update start.cmd

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -5,17 +5,17 @@ SET RANK_CALCULATOR_PATH=%~dp0..\RankCalculator\
 SET EVENTS_LOGGER_PATH=%~dp0..\EventsLogger\
 SET NGINX_PATH=%~dp0..\nginx\
 
-/d%VALUATOR_PATH% dotnet build
-/d%RANK_CALCULATOR_PATH% dotnet build
-/d%EVENTS_LOGGER_PATH% dotnet build
+%VALUATOR_PATH% dotnet build
+%RANK_CALCULATOR_PATH% dotnet build
+%EVENTS_LOGGER_PATH% dotnet build
 
-start /d%VALUATOR_PATH% dotnet run --no-build --urls "http://localhost:5001"
-start /d%VALUATOR_PATH% dotnet run --no-build --urls "http://localhost:5002"
+start /d %VALUATOR_PATH% dotnet run --no-build --urls "http://localhost:5001"
+start /d %VALUATOR_PATH% dotnet run --no-build --urls "http://localhost:5002"
 
-start /d%RANK_CALCULATOR_PATH% dotnet run --no-build
-start /d%RANK_CALCULATOR_PATH% dotnet run --no-build
+start /d %RANK_CALCULATOR_PATH% dotnet run --no-build
+start /d %RANK_CALCULATOR_PATH% dotnet run --no-build
 
-start /d%EVENTS_LOGGER_PATH% dotnet run --no-build
-start /d%EVENTS_LOGGER_PATH% dotnet run --no-build
+start /d %EVENTS_LOGGER_PATH% dotnet run --no-build
+start /d %EVENTS_LOGGER_PATH% dotnet run --no-build
 
 start /d%NGINX_PATH% nginx.exe
