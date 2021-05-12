9afcc88746e0deb0b039f451efaaccb0c8fd0e0a
IrinaZagaynova <ayrin.zagaynova@gmail.com>
2021-05-12 21:08:08 +0300

    Revert "Добавила 7 лабораторную"
    This reverts commit 98dad8134b9d0a3fe538bbca47681f0e84e80db3.

--- Chain/Arguments.cs
+++
@@ -1,7 +0,0 @@
-    struct Arguments
-    {
-        public int ListeningPort { get; set;}
-        public string NextHost { get; set; }
-        public int NextPort { get; set; }
-        public bool IsInitiator { get; set; }
-    }
